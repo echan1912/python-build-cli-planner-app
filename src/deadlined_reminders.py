a500de38a94bb7fbfdd8e37a2570f495c0f76d15
echan1912 <echan1912@gmail.com>
2021-04-29 19:31:41 +0800

    help

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -1,4 +1,4 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABCMeta, abstractmethod, ABC
 from collections.abc import Iterable
 
 class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
